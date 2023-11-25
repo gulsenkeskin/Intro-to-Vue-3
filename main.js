a8de0140b5434655b2651f9cf09152d436a54632
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-11-25 23:08:07 +0300

    l6

--- main.js
+++ main.js
@@ -9,12 +9,23 @@ const app = Vue.createApp({
       inventory: 100,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
-
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
       sizes: ["xs", "s", "m", "l", "xl"],
+      cart: 0,
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    decraseToCart() {
+      this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
