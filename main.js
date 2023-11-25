eba28a939a6c24b21b270210de060ea543b1eb05
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-11-25 22:55:59 +0300

    L3-end

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "açıklama",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://images.unsplash.com/photo-1585499583264-491df5142e83?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c29ja3N8ZW58MHx8MHx8fDA%3D",
     };
   },
 });
