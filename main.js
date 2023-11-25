c1b6e6407167eac718d7c99d4a3c81575a21c25a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-11-25 23:01:08 +0300

    l5

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ const app = Vue.createApp({
       inStock: true,
       inventory: 100,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["xs", "s", "m", "l", "xl"],
     };
   },
 });
